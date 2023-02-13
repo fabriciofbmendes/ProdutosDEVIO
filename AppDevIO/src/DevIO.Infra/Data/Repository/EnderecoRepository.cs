00abc574b4e892a2a1ba4ff38824d8712c1aa57f
Fabricio Mendes <fabriciofbmendes5@gmail.com>
2023-02-13 11:23:36 -0300

    Adicionando Repositorios Especializados

---
+++ AppDevIO/src/DevIO.Infra/Data/Repository/EnderecoRepository.cs
@@ -0,0 +1,17 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Infra.Data.Repository
+{
+    public class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
+    {
+        public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            return await ObterPorId(fornecedorId);
+        }
+    }
+}
