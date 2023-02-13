05f111f5eae1902904533af04265cd2139954dca
Fabricio Mendes <fabriciofbmendes5@gmail.com>
2023-02-13 15:34:41 -0300

    Services do Produto

---
+++ AppDevIO/src/ProdutosDevIO/Controllers/FornecedoresController.cs
@@ -0,0 +1,38 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using DevIO.Business.Models.Fornecedores.Services;
+using DevIO.Infra.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProdutosDevIO.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        private readonly IFornecedorService _fornecedorService;
+
+        public FornecedoresController()
+        {
+            _fornecedorService = new FornecedorService(new FornecedorRepository(),new EnderecoRepository());
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var fornecedor = new Fornecedor()
+            {
+                Nome = "",
+                Documento = "11111",
+                Endereco = new Endereco(),
+                TipoFornecedor = TipoFornecedor.PessoaFisica,
+                Ativo = true
+            };
+
+            await _fornecedorService.Adicionar(fornecedor);
+
+            return new EmptyResult();
+        }
+    }
+}
