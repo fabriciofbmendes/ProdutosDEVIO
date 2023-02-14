83fa8e845fd6dfc7772a9aba4538fbca3738d361
Fabricio Mendes <fabriciofbmendes5@gmail.com>
2023-02-14 13:55:32 -0300

    Modelando as ViewModels

---
+++ AppDevIO/src/ProdutosDevIO/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,50 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProdutosDevIO.ViewModels
+{
+    public class ProdutoViewModel
+    {
+
+        public ProdutoViewModel()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage ="O campo {0} e obrigatorio")]
+        [DisplayName("Fornecedor")]
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+        [StringLength(200,ErrorMessage ="O campo {0} precisa ter entre {2} e {1} caracteres",MinimumLength =2)]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        [DisplayName("Descricao")]
+        public string Descricao { get; set; }
+
+        [DisplayName("Imagem de Produto")]
+        public HttpPostedFileBase ImagemUpload { get; set; }
+        public string Imagem { get; set; }
+
+
+        [Required(ErrorMessage ="O campo {0} e obrigatorio")]
+        public decimal Valor { get; set; }
+        [ScaffoldColumn(false)]
+        public DateTime DataCadastro { get; set; }
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+
+        public FornecedorViewModel Fornecedor { get; set; }
+        public IEnumerable<FornecedorViewModel> Fornecedores { get; set; }
+
+    }
+}
