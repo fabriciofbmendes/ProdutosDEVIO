da0e763315f8f5309ee03adcd71d93f53ff43c0c
Fabricio Mendes <fabriciofbmendes5@gmail.com>
2023-02-15 13:43:58 -0300

    Configurando a Startup

--- AppDevIO/src/ProdutosDevIO/Startup.cs
+++ AppDevIO/src/ProdutosDevIO/Startup.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using ProdutosDevIO.App_Start;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
 
 [assembly: OwinStartupAttribute(typeof(ProdutosDevIO.Startup))]
 namespace ProdutosDevIO
@@ -9,6 +13,12 @@ namespace ProdutosDevIO
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            DependencyInjectionConfig.RegisterDIContainer();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
