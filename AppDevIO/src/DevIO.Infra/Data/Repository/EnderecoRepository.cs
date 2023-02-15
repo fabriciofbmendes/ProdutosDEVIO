e0ba03db6f7fbcaf6c046ad5ae673010c19864d7
Fabricio Mendes <fabriciofbmendes5@gmail.com>
2023-02-15 13:32:45 -0300

    Configurando SimpleInjector

--- AppDevIO/src/DevIO.Infra/Data/Repository/EnderecoRepository.cs
+++ AppDevIO/src/DevIO.Infra/Data/Repository/EnderecoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.Business.Models.Fornecedores;
+using DevIO.Infra.Data.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,9 @@ namespace DevIO.Infra.Data.Repository
 {
     public class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
     {
+
+
+        public EnderecoRepository(MeuDbContext context) : base(context) { }
         public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
         {
             return await ObterPorId(fornecedorId);
--- AppDevIO/src/ProdutosDevIO/Controllers/ProdutosController.cs
+++ AppDevIO/src/ProdutosDevIO/Controllers/ProdutosController.cs
@@ -19,12 +19,14 @@ namespace ProdutosDevIO.Controllers
             private readonly IProdutoService _produtoService;
             private readonly IMapper _mapper;
 
-            public ProdutosController()
-            {
-                
-            }
+        public ProdutosController(IProdutoRepository produtoRepository, IProdutoService produtoService, IMapper mapper)
+        {
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+            _mapper = mapper;
+        }
 
-            [Route("lista-de-produtos")]
+        [Route("lista-de-produtos")]
             [HttpGet]
             public async Task<ActionResult> Index()
             {
