d835fff5b7170408044ca35e088b27440e988b3d
Fabricio Mendes <fabriciofbmendes5@gmail.com>
2023-02-16 20:30:57 -0300

    DropDownList & Views Produtos
    Implementando DropDownList e Ajustando as Views de Produtos

--- AppDevIO/src/ProdutosDevIO/Controllers/ProdutosController.cs
+++ AppDevIO/src/ProdutosDevIO/Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using DevIO.Business.Core.Notificacoes;
+using DevIO.Business.Models.Fornecedores;
 using DevIO.Business.Models.Produtos;
 using DevIO.Business.Models.Produtos.Services;
 using DevIO.Infra.Data.Repository;
@@ -15,124 +16,136 @@ namespace ProdutosDevIO.Controllers
 {
     public class ProdutosController : Controller
     {
-            private readonly IProdutoRepository _produtoRepository;
-            private readonly IProdutoService _produtoService;
-            private readonly IMapper _mapper;
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
 
-        public ProdutosController(IProdutoRepository produtoRepository, IProdutoService produtoService, IMapper mapper)
+        public ProdutosController(IProdutoRepository produtoRepository, IProdutoService produtoService, IMapper mapper, IFornecedorRepository fornecedorRepository)
         {
             _produtoRepository = produtoRepository;
             _produtoService = produtoService;
+            _fornecedorRepository = fornecedorRepository;
             _mapper = mapper;
         }
 
         [Route("lista-de-produtos")]
-            [HttpGet]
-            public async Task<ActionResult> Index()
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            return View(_mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterProdutosFornecedores()));
+        }
+
+        [Route("dados-do-produto/{id:guid}")]
+        [HttpGet]
+        public async Task<ActionResult> Details(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel == null)
             {
-                return View(_mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterTodos()));
+                return HttpNotFound();
             }
 
-            [Route("dados-do-produto/{id:guid}")]
-            [HttpGet]
-            public async Task<ActionResult> Details(Guid id)
+            return View(produtoViewModel);
+        }
+
+        [Route("novo-produto")]
+        [HttpGet]
+        public async Task<ActionResult> Create()
+        {
+            var produtoViewModel = await PopularFornecedores(new ProdutoViewModel());
+
+            return View(produtoViewModel);
+        }
+
+        [Route("novo-produto")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProdutoViewModel produtoViewModel)
+        {
+            produtoViewModel = await PopularFornecedores(produtoViewModel);
+
+            if (ModelState.IsValid)
             {
-                var produtoViewModel = await ObterProduto(id);
-
-                if (produtoViewModel == null)
-                {
-                    return HttpNotFound();
-                }
-
-                return View(produtoViewModel);
-            }
-
-            [Route("novo-produto")]
-            [HttpGet]
-            public ActionResult Create()
-            {
-                return View();
-            }
-
-            [Route("novo-produto")]
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<ActionResult> Create(ProdutoViewModel produtoViewModel)
-            {
-                if (ModelState.IsValid)
-                {
-                    await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
-                    return RedirectToAction("Index");
-                }
-
-                return View(produtoViewModel);
-            }
-
-            [Route("editar-produto/{id:guid}")]
-            [HttpGet]
-            public async Task<ActionResult> Edit(Guid id)
-            {
-                var produtoViewModel = await ObterProduto(id);
-
-                if (produtoViewModel == null)
-                {
-                    return HttpNotFound();
-                }
-
-                return View(produtoViewModel);
-            }
-
-            [Route("editar-produto/{id:guid}")]
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<ActionResult> Edit(ProdutoViewModel produtoViewModel)
-            {
-                if (ModelState.IsValid)
-                {
-                    await _produtoService.Atualizar(_mapper.Map<Produto>(produtoViewModel));
-                    return RedirectToAction("Index");
-                }
-
-                return View(produtoViewModel);
-            }
-
-
-            [Route("excluir-produto/{id:guid}")]
-            [HttpGet]
-            public async Task<ActionResult> Delete(Guid id)
-            {
-                var produtoViewModel = await ObterProduto(id);
-
-                if (produtoViewModel == null)
-                {
-                    return HttpNotFound();
-                }
-
-                return View(produtoViewModel);
-            }
-
-            [Route("excluir-produto/{id:guid}")]
-            [HttpPost, ActionName("Delete")]
-            [ValidateAntiForgeryToken]
-            public async Task<ActionResult> DeleteConfirmed(Guid id)
-            {
-                var produtoViewModel = await ObterProduto(id);
-
-                if (produtoViewModel == null)
-                {
-                    return HttpNotFound();
-                }
-                await _produtoService.Remover(id);
-
+                await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
                 return RedirectToAction("Index");
             }
+
+            return View(produtoViewModel);
+        }
+
+        [Route("editar-produto/{id:guid}")]
+        [HttpGet]
+        public async Task<ActionResult> Edit(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(produtoViewModel);
+        }
+
+        [Route("editar-produto/{id:guid}")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(ProdutoViewModel produtoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await _produtoService.Atualizar(_mapper.Map<Produto>(produtoViewModel));
+                return RedirectToAction("Index");
+            }
+
+            return View(produtoViewModel);
+        }
+
+
+        [Route("excluir-produto/{id:guid}")]
+        [HttpGet]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(produtoViewModel);
+        }
+
+        [Route("excluir-produto/{id:guid}")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            await _produtoService.Remover(id);
+
+            return RedirectToAction("Index");
+        }
 
             private async Task<ProdutoViewModel> ObterProduto(Guid id)
             {
                 var produto = _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterProdutoFornecedor(id));
+                produto.Fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
                 return produto;
             }
 
+            private async Task<ProdutoViewModel> PopularFornecedores(ProdutoViewModel produto)
+            {
+                produto.Fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+                return produto;
+            }
             protected override void Dispose(bool disposing)
             {
                 if (disposing)
