7ee40ab9642fd8b91c464e3b60c145d64755ba79
Fabricio Mendes <fabriciofbmendes5@gmail.com>
2023-02-17 17:52:25 -0300

    Tratamento de Erros

---
+++ AppDevIO/src/ProdutosDevIO/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProdutosDevIO.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public int ErroCode { get; set; }
+        public string Titulo { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
