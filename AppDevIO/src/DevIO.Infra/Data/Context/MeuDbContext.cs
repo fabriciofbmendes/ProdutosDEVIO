c9f9a1287ee41e3575f80a0f686cee81d8007065
Fabricio Mendes <fabriciofbmendes5@gmail.com>
2023-02-17 12:28:50 -0300

    Definindo data automatica no EF

--- AppDevIO/src/DevIO.Infra/Data/Context/MeuDbContext.cs
+++ AppDevIO/src/DevIO.Infra/Data/Context/MeuDbContext.cs
@@ -7,6 +7,7 @@ using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DevIO.Infra.Data.Context
@@ -40,6 +41,24 @@ namespace DevIO.Infra.Data.Context
 
             base.OnModelCreating(modelBuilder);
 
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            foreach(var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
